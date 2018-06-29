afaf0d3b804f9f2c4e9273d0b5793a9abeb9afd1
Micah Shute <micah.shute@gmail.com>
2018-06-29 04:16:15 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    found = self.find_by_name
+    found = self.find_by_name(name)
     !!found ? found : self.create_by_name(name)
   end
 
